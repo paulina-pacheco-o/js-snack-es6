03af516f78fe7a4df0e8c1d8a4496e6c452a16b2
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-14 15:31:15 +0200

    array snack 1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const bici = [
+  {
+    nome: "Van Rysel"
+    peso: 10, 6 
+  },
+  {
+    nome: "Triban"
+    peso: 11, 25 
+  },
+  {
+    nome: "Sava"
+    peso: 8, 7
+  },
+  {
+    nome: "Cervel"
+    peso: 8, 4
+  },
+  {
+    nome: "Pinarello"
+    peso: 9, 42
+  },
+  {
+    nome: "Ridley"
+    peso: 8, 0
+  },
+]
